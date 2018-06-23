e4fcd4be3c24d6f1047f8e96bc0db45ef3a376ae
Panda <mingkaityler@gmail.com>
2018-06-23 11:22:04 +0800

    StringManipulationExercise(FundamentalsPart3)

---
+++ StringManipulationExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StringManipulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tvShow = "Game of thrones      ";
+            string seasonOne = " - Season 1";
+            string newName = "";
+
+            //Add " - Season 1"
+            //without you determining the start index
+
+            newName = tvShow.Insert(tvShow.Length, seasonOne);
+            Console.WriteLine(newName);
+            newName = tvShow.Insert(tvShow.Trim().Length, seasonOne);
+            Console.WriteLine(newName);
+        }
+    }
+}
