c9687c1a574ab8566a941e7cdf0903a23267b1cb
Panda <mingkaityler@gmail.com>
2018-06-23 22:28:43 +0800

    RandomNumbers(FundamentalsPArt3)

---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int x = 0;
+            x = rand.Next(1, 13);
+            Console.WriteLine("Month : " + x);
+
+            double y = 0;
+            y = rand.NextDouble();
+            Console.WriteLine("Random double no is " + Math.Round(y, 2));
+        }
+    }
+}
