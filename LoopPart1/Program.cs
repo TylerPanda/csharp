c93c25b01d103d49c6f47c7bcb19c6c231d55f0a
Panda <mingkaityler@gmail.com>
2018-06-23 12:24:56 +0800

    LoopPart1(FundamentalsPart3)

---
+++ LoopPart1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LoopPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("========== Increment ==========");
+            for (int i = 0 ; i <= 10 ; i++)
+            {
+                Console.WriteLine("The value of variable is " + i);
+            }
+            Console.WriteLine("========== Decrease ==========");
+            for (int i = 10 ; i>=1 ; i--)
+            {
+                Console.WriteLine("The value of variable is " + i);
+            }
+            Console.WriteLine("========== Break ==========");
+            for(int i = 10 ; i >= 1 ; i--)
+            {
+                if(i == 2)
+                {
+                    break;
+                }
+                Console.WriteLine("The value of variable is " + i);
+            }
+        }
+    }
+}
