07970ce958087dabba620b3560a0145b6101ef0a
Panda <mingkaityler@gmail.com>
2018-06-23 12:41:10 +0800

    LoopPart2_While_and_DoWhile)FundamentalsPart3)

---
+++ LoopPart2_While_and_DoWhile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LoopPart2_While_and_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("while Loop");
+            int x = 8;
+
+            while(x >= 1)
+            {
+                if(x == 3)
+                {
+                    break;    
+                }
+                Console.WriteLine("Game of thrones season 0" + x);
+                x--;
+            }
+            Console.WriteLine("----------");
+            Console.WriteLine("Do while Loop");
+
+            int i = 0;
+            do
+            {
+                Console.WriteLine("This is the line no." + i);
+                i++;
+            } while (i <= 10);
+        }
+    }
+}
