04bcbdefde3f854389ba83ae7b3256630fdd97ef
Panda <mingkaityler@gmail.com>
2018-06-23 12:08:35 +0800

    StringInterpolation(FundamentalsPart3)

---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "";
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Welcome back " + name);
+            Console.WriteLine("Welcome Back {0}", name);
+            Console.WriteLine($"Welcome back again {name}");
+        }
+    }
+}
