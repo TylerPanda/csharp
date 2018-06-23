4e92eded575df7e8b1510d31734145241450e47f
Panda <mingkaityler@gmail.com>
2018-06-23 09:28:50 +0800

    Constants(FundamentalsPart3)

---
+++ Constants/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double gravityForce = 9.8;
+            double PI = 3.14159265359;
+
+            gravityForce = 1;
+
+            Console.WriteLine("Gravity force is " + gravityForce + " and PI is " + PI);
+        }
+    }
+}
