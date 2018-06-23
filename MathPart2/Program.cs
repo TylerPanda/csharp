800870f49ff0a1b0d3470dcd43e65e8f57802599
Panda <mingkaityler@gmail.com>
2018-06-23 21:00:12 +0800

    MathPart2(FundamentalsPart3)

---
+++ MathPart2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MathPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 9;
+            int y = 4;
+
+            // Max
+            Console.WriteLine("Max: " + Math.Max(x, y));
+
+            //Min
+            Console.WriteLine("Min: " + Math.Min(x, y));
+
+            //Sqrt
+            Console.WriteLine("Sqrt:" + Math.Sqrt(x));
+
+            //Pow
+            Console.WriteLine("Pow: " + Math.Pow(x, y));
+
+            //PI
+            Console.WriteLine("PI: " + Math.PI);
+        }
+    }
+}
