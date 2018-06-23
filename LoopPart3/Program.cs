d5a973fd6a4b8d828521b29cc57a9619d3650818
Panda <mingkaityler@gmail.com>
2018-06-23 15:37:12 +0800

    LoopPart3(FundamentalsPart3)

---
+++ LoopPart3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopPart3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1 ; i <= 5 ; i++)
+            {
+                Console.WriteLine("Frist Loop i = " + i);
+                for(int j = 1 ; j <= 4 ; j++)
+                {
+                    Console.WriteLine("Second Loop j = " + j);
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 1 ; i <= 30 ; i++)
+            {
+                for(int j = 1 ; j <= 30 ; j++)
+                {
+                    Console.Write("0 ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
