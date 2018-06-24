56456379f02ab6bef8f9baae53c8fb94b98fff06
Panda <mingkaityler@gmail.com>
2018-06-25 07:34:51 +0800

    OverloadedMethodExercise(Object-Oriented_Part1)

---
+++ OverloadedMethodExercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace OverloadedMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            double x, y, z;
+
+            // intResult = 0;
+            a = 1;
+            b = 2;
+            c = 3;
+
+            // doubleResult = 0;
+            x = 0.50;
+            y = 1.50;
+            z = 2.0;
+
+            Console.WriteLine( PerformAdd(a, b));
+            Console.WriteLine( PerformAdd(a, b, c));
+            Console.WriteLine( PerformAdd(x, y));
+            Console.WriteLine( PerformAdd(x, y, z));
+        }
+        static int PerformAdd(int num1, int num2)
+        {
+            return num1 + num2; 
+        }
+
+        static int PerformAdd(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+        static double PerformAdd(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        static double PerformAdd(double num1, double num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+    }
+}
