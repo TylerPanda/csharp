1983a3f46a438acf914ad082fde94502eac395c5
Panda <mingkaityler@gmail.com>
2018-06-24 22:18:03 +0800

    MethodPart3(Object-Oriented_Part1)

---
+++ MethodPart3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodPart3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstEmployee, secondEmployee;
+
+            firstEmployee = "David Smith";
+            secondEmployee = "Sophia Waston";
+
+            Console.WriteLine($"Inside Main Method\n----------\n{firstEmployee}\n{secondEmployee}\n\n");
+            changNames(ref firstEmployee, ref secondEmployee);
+
+            Console.WriteLine($"Inside Main Method\n----------\n{firstEmployee}\n{secondEmployee}\n\n");
+        }
+
+        static void changNames(ref string firstEmployee, ref string secondEmployee)
+        {
+            firstEmployee = "Olivia Aaron";
+            secondEmployee = "Alvaro Salazar";
+            Console.WriteLine($"Outside Main Method\n----------\n{firstEmployee}\n{secondEmployee}\n\n");
+        }
+    }
+}
