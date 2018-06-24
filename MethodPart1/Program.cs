52c0395e9f18d4997397d4ee08c717a888ee9c0a
Panda <mingkaityler@gmail.com>
2018-06-24 20:51:53 +0800

    MethodPart1(Object-Oriented_Part1)

---
+++ MethodPart1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MethodPart1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            int addResult = 0;
+
+            a = 5;
+            b = 3;
+            addResult = a + b;
+            Console.WriteLine($"{a} + {b} = {addResult}");
+            displayMessages();
+
+            c = 10;
+            d = 15;
+            addResult = c + d;
+            Console.WriteLine($"{c} + {d} = {addResult}");
+            displayMessages();
+        }
+
+        static void displayMessages()
+        {
+            Console.WriteLine("Process is done.");
+            Console.WriteLine("This process is run by ahmad.");
+            Console.WriteLine("Finished on time : " + DateTime.Now.ToShortTimeString());
+        }
+    }
+}
