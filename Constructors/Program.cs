84669d0ebc050b905f2459d3843eeee632067af7
Panda <mingkaityler@gmail.com>
2018-06-25 07:58:40 +0800

    Constructors(Object-Oriented_Part1)

---
+++ Constructors/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person personOne = new Person();
+            personOne.FirstName = "Ahmad";
+            personOne.LastName = "Mohey";
+            personOne.Country = "Egypt";
+
+            Person personTwo = new Person();
+            personTwo.FirstName = "Tim";
+            personTwo.LastName = "David";
+            personTwo.Country = "Australia";
+
+            Person personThree = new Person();
+
+            Console.WriteLine(personOne.FirstName);
+            Console.WriteLine(personTwo.FirstName);
+            Console.WriteLine(personThree.FirstName);
+        }
+    }
+
+    class Person
+    {
+        string firstName;
+        string lastName;
+        DateTime birthday;
+        string country;
+
+        public string FirstName
+        {
+            get{ return firstName; }
+            set{ firstName = value; }
+        }
+
+        public string LastName
+        {
+            get{ return lastName; }
+            set{ lastName = value; }
+        }
+
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+
+        public string Country
+        {
+            get;
+            set;
+        }
+
+        public Person()
+        {
+            firstName = "Unknown";
+        }
+    }
+}
