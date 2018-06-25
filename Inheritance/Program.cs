48182e0e50aceabc7e7d97d61a846f283f2552dc
Panda <mingkaityler@gmail.com>
2018-06-25 21:23:22 +0800

    Inherentance(Object-Oriented_Part1)

---
+++ Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dogs dog = new Dogs();
+            Console.WriteLine(dog);
+        }
+    }
+    class Animal
+    {
+        public string animalName;
+        public DateTime animalBirthDate;
+
+        public void FeedAnimal()
+        {
+
+        }
+    }
+
+    class Dogs
+    {
+        public string dogBreed;
+        public string dogIntelligence;
+        public bool isEasyToString;
+    }
+}
