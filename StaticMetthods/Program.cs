8a2fa864508f66902e0667acee76870a22488f5c
Panda <mingkaityler@gmail.com>
2018-06-25 08:13:19 +0800

    StaticMethods(Object-Oriented_Part1)

---
+++ StaticMetthods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StaticMetthods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.SayHi();
+            SayHi();
+            
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hi, from the static method.");
+        }
+    }
+
+    class Person
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi, from non the static method.");
+        }
+    }
+}
