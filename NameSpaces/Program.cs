792a2dd56f77f5a7be1bf671c13d54b0f9a3a3fe
Panda <mingkaityler@gmail.com>
2018-06-26 07:51:30 +0800

    NameSpaces(Object-Oriented_Part2)

---
+++ NameSpaces/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MyOwnNameSpace.GameOfThrone;
+
+namespace NameSpaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            MyOwnNameSpace.animals animal = new MyOwnNameSpace.animals();
+            animal.sayHi();
+            // MyOwnNameSpace.GameOfThrone.HouseStark.sayQuote();
+            HouseStark.sayQuote();
+        }
+    }
+}
+
+namespace MyOwnNameSpace
+{
+    class animals
+    {
+        public string animalColor = "animalColor";
+
+        public void sayHi()
+        {
+            Console.WriteLine("Hi, from the custome namespace.");
+        }
+    }
+    namespace GameOfThrone
+    {
+        class HouseStark
+        {
+            public static void sayQuote()
+            {
+                Console.WriteLine("The north remembers.");
+            }
+        }
+    }
+} 
