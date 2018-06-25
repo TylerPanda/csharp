42e7f769a569c8638ce938cf5723a902dda0f825
Panda <mingkaityler@gmail.com>
2018-06-26 07:21:30 +0800

    NestClasses(Object-Oriented-Part2)

---
+++ NestClasses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NestClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals animal = new Animals();
+            Animals.Dogs dog = new Animals.Dogs();
+            Animals.Cats cat = new Animals.Cats();
+            
+            Console.WriteLine(animal.animalName + "\t" +  animal.animalCountry);
+            Console.WriteLine(dog.dogBreed + "\t" + dog.dogNickName);
+            Console.WriteLine(cat.catName);
+        }
+    }
+    class Animals
+    {
+        public string animalName = "animalName";
+        public string animalCountry = "animalCountry";
+
+        public class Dogs
+        {
+            public string dogBreed = "dogBreed";
+            public string dogNickName = "dogNickName";
+        }
+
+        public class Cats
+        {
+            public string catName = "catName";
+        }
+    }
+}
