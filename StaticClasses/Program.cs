a4d72f696a5af955374466d589d70185f54183ca
Panda <mingkaityler@gmail.com>
2018-06-26 06:59:35 +0800

    StaticClasses(Object-Oriented_Part2)

---
+++ StaticClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StaticClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal.GetAge();
+        }
+    }
+    static class Animal
+    {
+        static public string name;
+
+        static public void GetAge()
+        {
+            Console.WriteLine(DateTime.Now.Year - DateTime.Now.AddYears(-4).Year);
+        }
+    }
+}
