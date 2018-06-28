63dbb03e6fe8eb7edd0b6605496beed371fe63c5
Panda <mingkaityler@gmail.com>
2018-06-28 08:17:16 +0800

    Struct(Object-Oriented_Part2)

---
+++ Struct/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Employee newEmployee = new Employee();
+            newEmployee.employeeName = "James";
+            newEmployee.employeeJob = "Programmer";
+            newEmployee.Salary = 5000;
+            Console.WriteLine($"Employee name is {newEmployee.employeeName} and the job is {newEmployee.employeeJob} and salary is {newEmployee.Salary} ");
+            newEmployee.SayHi();
+        }
+    }
+
+    struct Employee
+    {
+        public string employeeName;
+        public string employeeJob;
+        private decimal salary;
+
+        public decimal Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hi, from the method in struct.");
+        }
+    }
+}
