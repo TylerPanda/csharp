47bb2c58feb2375f627c739c79f33ebdc3a4e791
Panda <mingkaityler@gmail.com>
2018-07-03 20:06:34 +0800

    Enumerations(Object-Oriented_Part2)

---
+++ Enumerations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Enumerations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WeekDay day = WeekDay.Friday;
+            Console.WriteLine(day);
+            Console.WriteLine((int)day);
+        }
+    }
+    enum WeekDay
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
