868f1fab3ad44b883e52670522693a2e044dd460
Panda <mingkaityler@gmail.com>
2018-07-04 07:21:35 +0800

    Keyword(Object-Oriented_Part2)

---
+++ Keywords/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Keywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.DisplayName("James", "Morrison");
+        }
+    }
+
+    class Person
+    {
+        string firstName;
+        string lastName;
+
+        public void DisplayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+
+        public void DisplayName(string firstName, string lastName)
+        {
+            Console.WriteLine(firstName + " " + lastName);
+            Console.WriteLine(this.firstName + " " + this.lastName);
+        }
+
+        public Person()
+        {
+            firstName = "Not yet determined!";
+            lastName = string.Empty;
+        }
+    }
+}
