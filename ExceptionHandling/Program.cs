a41d712a26e170b7a0c428667fbb1bb0a4188790
Panda <mingkaityler@gmail.com>
2018-07-04 07:37:08 +0800

    ExceptionHandling(Object-Oriented_Part2)

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 9;
+            double div = 0;
+
+            try
+            {
+                div = y / x;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error is : " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Result is : " + div);
+            }
+        }
+    }
+}
